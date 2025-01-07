d0bbc6eb7b8f6533f1d615ffba7d3631687ebf85
martilut <jeykolton@gmail.com>
2025-01-08 01:51:14 +0300

    Added PRELIM filter

--- ms/meta_dataset/metadata_filter.py
+++ ms/meta_dataset/metadata_filter.py
@@ -1,23 +1,24 @@
 from abc import ABC, abstractmethod
+
+import numpy as np
 import pandas as pd
 
 
-class FeaturesFilter(ABC):
+class MetadataFilter(ABC):
     name: str = "base_filter"
     def __init__(self):
-        ...
+        pass
 
     @abstractmethod
-    def __filter__(self, features_dataset: pd.DataFrame) -> pd.DataFrame:
-        ...
+    def filter_features(self, features_dataset: pd.DataFrame) -> pd.DataFrame:
+        pass
+
+    @abstractmethod
+    def filter_target(self, target_dataset: pd.DataFrame) -> pd.DataFrame:
+        pass
 
 
-    def filter(self, features_dataset: pd.DataFrame) -> tuple[pd.DataFrame, int]:
-        filtered_features = self.__filter__(features_dataset=features_dataset)
-        return filtered_features, filtered_features.shape[1]
-
-
-class TabzillaFilter(FeaturesFilter):
+class TabzillaFilter(MetadataFilter):
     name = "tabzilla_filter"
 
     def __init__(
@@ -29,6 +30,8 @@ class TabzillaFilter(FeaturesFilter):
             features_to_exclude: list[str] | None = None,
             keys_to_exclude: list[str] | None = None,
             datasets_to_exclude: list[str] | None = None,
+            models_to_exclude: list[str] | None = None,
+            models_priority_list: list[str] | None = None,
     ):
         super().__init__()
         self.dataset_name = dataset_name
@@ -46,15 +49,19 @@ class TabzillaFilter(FeaturesFilter):
         self.features_to_exclude = features_to_exclude
         self.keys_to_exclude = keys_to_exclude
         self.datasets_to_exclude = datasets_to_exclude
+        self.models_to_exclude = models_to_exclude
+        self.models_priority_list = models_priority_list
 
-    def __filter__(self, features_dataset: pd.DataFrame) -> pd.DataFrame:
+    def filter_features(self, features_dataset: pd.DataFrame) -> pd.DataFrame:
         agg_features = features_dataset.groupby(self.dataset_name).median(numeric_only=True)
         for attr in self.round_attrs:
             agg_features.loc[:, attr] = agg_features[attr].round(0)
 
         num_datasets = len(agg_features.index)
         for col in agg_features:
-            if agg_features[col].isna().sum() > num_datasets * self.nan_threshold:
+            x = agg_features[col].to_numpy()
+            if (agg_features[col].isna().sum() > num_datasets * self.nan_threshold
+                    or np.all(x == x[0])):
                 agg_features.drop(col, axis="columns", inplace=True)
 
         agg_features = agg_features.fillna(
@@ -70,9 +77,8 @@ class TabzillaFilter(FeaturesFilter):
             ]
             agg_features = agg_features[filter_cols]
 
-        if self.features_to_exclude is not None:
-            new_features = [f for f in agg_features.cols if f not in self.features_to_exclude]
-            agg_features = agg_features.loc[:, new_features]
+        agg_features.drop(self.features_to_exclude, axis="columns", inplace=True)
+        agg_features.drop(self.datasets_to_exclude, axis="index", inplace=True)
         if self.keys_to_exclude is not None:
             new_features = []
             for f in agg_features.col:
@@ -84,8 +90,17 @@ class TabzillaFilter(FeaturesFilter):
                 if is_included:
                     new_features.append(f)
             agg_features = agg_features.loc[:, new_features]
-        if self.datasets_to_exclude is not None:
-            new_datasets = [r for r in agg_features.index if r not in self.datasets_to_exclude]
-            agg_features = agg_features.loc[new_datasets, :]
 
         return agg_features
+
+    def filter_target(self, target_dataset: pd.DataFrame) -> pd.DataFrame:
+        res_target = target_dataset.copy()
+        res_target.drop(self.models_to_exclude, axis="columns", inplace=True)
+        priority_list = self.models_priority_list if self.models_priority_list is not None \
+            else list(res_target.columns)
+        for model in priority_list:
+            for row in res_target.index:
+                if np.isnan(res_target.loc[row, model]):
+                    res_target.drop(row, axis="rows", inplace=True)
+
+        return res_target
