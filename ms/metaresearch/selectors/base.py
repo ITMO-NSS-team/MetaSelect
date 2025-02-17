ed0ea5d7e4ab0f87939984d47a389cf4eb22d9e0
martilut <jeykolton@gmail.com>
2025-02-17 23:19:44 +0300

    Added base feature selector (without selection)

---
+++ ms/metaresearch/selectors/base.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+
+from ms.handler.metadata_source import MetadataSource
+from ms.handler.selector_handler import SelectorHandler
+from ms.utils.typing import NDArrayFloatT
+
+
+class BaseSelector(SelectorHandler):
+    @property
+    def class_folder(self) -> str:
+        return "no_selector"
+
+    @property
+    def class_name(self) -> str:
+        return "no_selector"
+
+    def __init__(
+            self,
+            md_source: MetadataSource,
+            features_folder: str = "preprocessed",
+            metrics_folder: str | None = "preprocessed",
+            test_mode: bool = False,
+    ) -> None:
+        super().__init__(
+            md_source=md_source,
+            features_folder=features_folder,
+            metrics_folder=metrics_folder,
+            test_mode=test_mode,
+        )
+
+    def handle_data(
+            self,
+            x: NDArrayFloatT,
+            y: NDArrayFloatT,
+            features_names: list[str],
+    ) -> pd.DataFrame:
+        res = pd.DataFrame(index=features_names)
+        res["no_selection"] = np.zeros(shape=(len(features_names),))
+        return res
